003a9b6da6533f170c0e0d9b6b80051298759157
Mehrshad Hamavandy <73274226+itsMehrshad@users.noreply.github.com>
2020-11-22 03:04:12 -0800

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -17,9 +17,9 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.speak = (function speak(name) {
-  console.log(speakWord + " " + byeSpeaker.name);
-});
+(function speak(name) {
+byeSpeaker.speak = console.log(speakWord + " " + byeSpeaker.name);
+})();
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
