3e1ab2f0c76d8e339e05a5eeab90b2ea63f1debe
Mehrshad Hamavandy <73274226+itsMehrshad@users.noreply.github.com>
2020-11-22 03:17:40 -0800

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -16,7 +16,7 @@ var speakWord = "Hello";
 // See Lecture 52, part 2
 
 helloSpeaker.speak = (function speak(name) {
-  console.log(speakWord + " " + helloSpeaker.speak(name));
+  console.log(speakWord + " " + helloSpeaker.name);
 })();
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
